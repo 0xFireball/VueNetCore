58ec24787b618cfe8f57413c2a8cf5785c880aec
0xFireball <0xFireball@outlook.com>
2016-12-11 20:20:35 -0800

    live build works!

--- VueNetCore/VueNetCore/ClientApp/components/Hello.vue
+++ VueNetCore/VueNetCore/ClientApp/components/Hello.vue
@@ -17,6 +17,9 @@
       <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
     </ul>
+      <h3>
+          {{ jssInfo }}
+      </h3>
   </div>
 </template>
 
@@ -25,7 +28,8 @@ export default {
   name: 'hello',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+        msg: 'Welcome to Your Vue.js App!',
+        jssInfo: 'This app is running on ASP.NET Core with JavaScriptServices!'
     }
   }
 }
